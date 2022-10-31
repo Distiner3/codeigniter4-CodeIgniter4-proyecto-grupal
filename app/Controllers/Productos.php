fd1367cc557f20bc4295521d896d8a4ca1d60de5
Distiner3 <distiner3@gmail.com>
2022-10-30 20:10:39 -0600

    Agregar datos en productos

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -13,4 +13,29 @@ class Productos extends Controller{
         return view('Productos', $registros);
     }
 
+    public function agregar_productos()
+    {
+        $producto = new Producto();
+
+        $id = $this->request->getVar('txt_id');
+        $marca = $this->request->getVar('txt_marca');
+        $talla = $this->request->getVar('txt_talla');
+        $colores = $this->request->getVar('txt_color');
+        $descripcion = $this->request->getVar('txt_descripcion');
+        $precio = $this->request->getVar('txt_precio');
+
+        $datos = [
+            'id_producto' => $id,
+            'marca' => $marca,
+            'talla' => $talla,
+            'color' => $colores,
+            'descripcion' => $descripcion,
+            'precio' => $precio,
+            ];
+        $producto->insert($datos);
+        $registros['productos'] = $producto->findAll();
+
+        return view('Productos', $registros);
+
+    }
 }
